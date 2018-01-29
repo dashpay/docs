296905dce245ca5a2ae953c30af8d2d8f2b3de67
strophy <leon.white@dash.org>
2018-01-29 19:12:34 +0800

    Move settings to new file

---
+++ _templates/local_settings.py
@@ -0,0 +1,8 @@
+import os
+
+html_theme = 'sphinx_rtd_theme'
+html_logo = 'img/dash-logo.svg'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
--- conf.py
+++ conf.py
@@ -16,10 +16,8 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-
-import sphinx_rtd_theme
+# import os
+# import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -88,14 +86,6 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_logo = 'img/dash-logo.svg'
-html_theme_options = {
-    'logo_only': True,
-    'display_version': False,
-}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
