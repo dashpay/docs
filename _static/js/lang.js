a4bba7c2d12ad6e7ab9ef4fac6b2f2a92e6af640
strophy <leon.white@dash.org>
2018-10-20 13:04:12 +0800

    Redirect logic

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-	$("#langselect").val(DOCUMENTATION_OPTIONS['LANGUAGE']);
+	$('#langselect').val(DOCUMENTATION_OPTIONS['LANGUAGE']);
+});
+
+$('#langselect').change(function(){
+	window.location.href = "https://docs.dash.org/" + $('#langselect').val() + "/latest/";
 });
