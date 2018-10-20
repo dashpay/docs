f47d9662ee481929918bdcb3e7c1da9f096ea5f6
strophy <leon.white@dash.org>
2018-10-20 19:54:50 +0800

    Point to different language version of same page on language selection

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
 });
 
 $('#langselect').change(function(){
-	window.location.href = "https://docs.dash.org/" + $('#langselect').val() + "/latest/";
+	var pageURL = $(location).attr("href");
+	pageURL = pageURL.replace("https://docs.dash.org/" + DOCUMENTATION_OPTIONS['LANGUAGE'], "")
+	window.location.href = "https://docs.dash.org/" + $('#langselect').val() + pageURL;
 });
