666664cb527f4e3a4650c055c585cae3f0054ce7
strophy <leon.white@dash.org>
2018-12-21 16:39:42 +0800

    Comment JS changes

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$('#langselect').val(DOCUMENTATION_OPTIONS['LANGUAGE']);
-	jQuery('link[rel="alternate"]').remove();
+	/* jQuery('link[rel="alternate"]').remove();
 	var link = document.createElement('link');
 	link.rel = "alternate";
 	link.hreflang = "x-default";
 	link.href = "https://docs.dash.org/en/latest/";
-	jQuery('head').append(link);
+	jQuery('head').append(link);*/
 });
 
 $('#langselect').change(function(){
