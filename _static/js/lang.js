b347b78b043e781935e29d06c0e2492d7ff2865c
strophy <leon.white@dash.org>
2018-12-24 17:13:10 +0800

    Add dynamic hreflang alternates

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
+	/* Select currently language */
 	$('#langselect').val(DOCUMENTATION_OPTIONS['LANGUAGE']);
+	
+	/* Set alternate links */
+	var langs = [ "de", "en", "es", "fr", "pt", "vi", "el", "ru", "ko", "ja", "zh-Hans", "zh-Hant", "ar", "x-default" ];
+	var pageURL = $(location).attr("href");
+	pageURL = pageURL.replace("https://docs.dash.org/" + DOCUMENTATION_OPTIONS['LANGUAGE'] , "");
+	$.each(langs, function(index, value) {
+		var link = document.createElement('link');
+		link.rel = "alternate";
+		link.hreflang = value;
+		if (value == "x-default") {
+			link.href = "https://docs.dash.org/en/" + pageURL;
+		} else if (value == "zh-Hans") {
+			link.href = "https://docs.dash.org/zh_CN" + pageURL;
+		} else if (value == "zh-Hant") {
+			link.href = "https://docs.dash.org/zh_TW" + pageURL;
+		} else {
+			link.href = "https://docs.dash.org/" + value + pageURL;
+		}
+		jQuery('head').append(link);
+	});
 });
 
 $('#langselect').change(function(){
 	var pageURL = $(location).attr("href");
-	pageURL = pageURL.replace("https://docs.dash.org/" + DOCUMENTATION_OPTIONS['LANGUAGE'], "")
+	pageURL = pageURL.replace("https://docs.dash.org/" + DOCUMENTATION_OPTIONS['LANGUAGE'], "");
 	window.location.href = "https://docs.dash.org/" + $('#langselect').val() + pageURL;
 });
