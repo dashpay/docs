ccd6bacb2193947de12ebf3c943d929b23b61c41
strophy <32928115+strophy@users.noreply.github.com>
2020-10-23 11:42:15 -0700

    add _static path

--- conf.py
+++ conf.py
@@ -119,7 +119,7 @@ html_logo = 'img/dash_logo_white.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
 
 # Override to allow text wrap in tables
 # Details: https://rackerlabs.github.io/docs-rackspace/tools/rtd-tables.html
