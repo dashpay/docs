ce68dfdfbb837b5c390cfee1eca223a830379dcb
strophy <badfunkstripe@gmail.com>
2021-06-24 22:16:46 +0200

    chore: update copyright for 2021

--- conf.py
+++ conf.py
@@ -41,8 +41,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Dash'
-copyright = u'2020, Dash Core Group, Inc'
-author = u'strophy'
+copyright = u'2021, Dash Core Group, Inc'
+author = u'strophy,thephez'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
