14521c94e2f1835289914467f069b856ec7575e4
thephez <thephez@users.noreply.github.com>
2023-06-19 10:20:27 -0400

    chore: add some missing ones

--- scripts/update-download-links.sh
+++ scripts/update-download-links.sh
@@ -1,2 +1,2 @@
 find . -iname "*.rst" -exec sed -i 's~/v19.1.0/dashcore-19.1.0-~/v19.2.0/dashcore-19.2.0-~g' {} +
-find . -iname "*.rst" -exec sed -i 's~ dashcore-19.1.0-~ dashcore-19.2.0-~g' {} +
+find . -iname "*.rst" -exec sed -i 's~dashcore-19.1.0-~dashcore-19.2.0-~g' {} +
