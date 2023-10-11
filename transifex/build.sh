3c7020fb414793663a0a23e0c1076ad59d4a17c4
thephez <thephez@users.noreply.github.com>
2023-10-11 13:03:27 -0400

    refactor: sort languages alphabetically

--- transifex/build.sh
+++ transifex/build.sh
@@ -1,6 +1,7 @@
 # Triggers a build of readthedocs sub-projects for each supported language
 # Token and webhook variables should be exported via a .env file
 curl -X POST -d "branches=stable" -d "token=$ARABIC" $ARABIC_WEBHOOK
+curl -X POST -d "branches=stable" -d "token=$FILIPINO" $FILIPINO_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$FRENCH" $FRENCH_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$GERMAN" $GERMAN_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$GREEK" $GREEK_WEBHOOK
@@ -13,4 +14,3 @@ curl -X POST -d "branches=stable" -d "token=$SIMPLIFIED_CHINESE" $SIMPLIFIED_CHI
 curl -X POST -d "branches=stable" -d "token=$SPANISH" $SPANISH_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$TRADITIONAL_CHINESE" $TRADITIONAL_CHINESE_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$VIETNAMESE" $VIETNAMESE_WEBHOOK
-curl -X POST -d "branches=stable" -d "token=$FILIPINO" $FILIPINO_WEBHOOK
