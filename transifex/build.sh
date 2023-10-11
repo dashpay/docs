6ea66dba18d2bfe3c22cfe1afaf6612f2204e56d
thephez <thephez@users.noreply.github.com>
2023-10-11 13:10:47 -0400

    feat: add webhook to build "it" (#306)
    * feat: add webhook for it
    
    * refactor: sort languages alphabetically

--- transifex/build.sh
+++ transifex/build.sh
@@ -1,9 +1,11 @@
 # Triggers a build of readthedocs sub-projects for each supported language
 # Token and webhook variables should be exported via a .env file
 curl -X POST -d "branches=stable" -d "token=$ARABIC" $ARABIC_WEBHOOK
+curl -X POST -d "branches=stable" -d "token=$FILIPINO" $FILIPINO_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$FRENCH" $FRENCH_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$GERMAN" $GERMAN_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$GREEK" $GREEK_WEBHOOK
+curl -X POST -d "branches=stable" -d "token=$ITALIAN" $ITALIAN_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$JAPANESE" $JAPANESE_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$KOREAN" $KOREAN_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$PORTUGUESE" $PORTUGUESE_WEBHOOK
@@ -12,4 +14,3 @@ curl -X POST -d "branches=stable" -d "token=$SIMPLIFIED_CHINESE" $SIMPLIFIED_CHI
 curl -X POST -d "branches=stable" -d "token=$SPANISH" $SPANISH_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$TRADITIONAL_CHINESE" $TRADITIONAL_CHINESE_WEBHOOK
 curl -X POST -d "branches=stable" -d "token=$VIETNAMESE" $VIETNAMESE_WEBHOOK
-curl -X POST -d "branches=stable" -d "token=$FILIPINO" $FILIPINO_WEBHOOK
