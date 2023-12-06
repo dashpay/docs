f1834db79d826ec804b6aa7b73579db7da816c74
thephez <thephez@users.noreply.github.com>
2023-12-06 16:26:46 -0500

    feat: pull translations directly from the api instead of hard-coding

--- transifex/build-status-check.sh
+++ transifex/build-status-check.sh
@@ -1,28 +1,18 @@
 # Checks build status for most recent builds of readthedocs sub-projects for each supported language
 # Token should be exported via a .env file
 
-# Define project slugs and their respective tokens
-declare -A projects
-projects[arabic]=dash-arabic
-projects[english]=dash-docs
-projects[filipino]=dash-filipino
-projects[french]=dash-french
-projects[german]=dash-german
-projects[greek]=dash-greek
-projects[italian]=dash-italian
-projects[japanese]=dash-japanese
-projects[korean]=dash-korean
-projects[portuguese]=dash-portuguese
-projects[russian]=dash-russian
-projects[simplified-chinese]=dash-simplified-chinese
-projects[spanish]=dash-spanish
-projects[traditional-chinese]=dash-traditional-chinese
-projects[vietnamese]=dash-vietnamese
+# Define the main project slug
+main_project_slug="dash-docs"
 
-# Loop through each project and fetch build status
-for lang in "${!projects[@]}"; do
-    slug=${projects[$lang]}
-    echo -e "\e[1m$lang ($slug) build status...\e[0m"
+# Fetch the main project's details including translations
+response=$(curl -s -H "Authorization: Token $RTD_TOKEN" "https://readthedocs.org/api/v3/projects/$main_project_slug/translations/?limit=20")
+
+# Loop through each translation project
+echo $response | jq -c '.results[]' | while read -r translation; do
+    slug=$(echo $translation | jq -r '.slug')
+    language_name=$(echo $translation | jq -r '.language.name')
+
+    echo -e "\e[1m$language_name ($slug) build status...\e[0m"
     
     # Make the API call
     response=$(curl -s -H "Authorization: Token $RTD_TOKEN" "https://readthedocs.org/api/v3/projects/$slug/builds/?limit=2")
