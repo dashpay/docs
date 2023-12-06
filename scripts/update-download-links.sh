1e78e31d290d3fdf87aff486ef654e170b4bd605
thephez <thephez@users.noreply.github.com>
2023-12-06 14:15:10 -0500

    chore: update download links for v20.0.2 (#323)
    * refactor: update script to use variables that are assigned once
    
    * chore: update script for v20.0.2
    
    * chore: update download links to 20.0.2

--- scripts/update-download-links.sh
+++ scripts/update-download-links.sh
@@ -2,6 +2,12 @@
 
 # Script to update the download/install related version references when a new
 # Dash Core version is released
-find . -iname "*.rst" -exec sed -i 's~/v19.3.0/dashcore-19.3.0-~/v20.0.0/dashcore-20.0.0-~g' {} +
-find . -iname "*.rst" -exec sed -i 's~dashcore-19.3.0-~dashcore-20.0.0-~g' {} +
-find . -iname "*.rst" -exec sed -i 's~dashcore-19.3.0~dashcore-20.0.0~g' {} +
+
+# Define old and new version variables
+OLD_VERSION="20.0.0"
+NEW_VERSION="20.0.2"
+
+# Use the variables in the find/sed commands
+find . -iname "*.rst" -exec sed -i "s~/v${OLD_VERSION}/dashcore-${OLD_VERSION}-~/v${NEW_VERSION}/dashcore-${NEW_VERSION}-~g" {} +
+find . -iname "*.rst" -exec sed -i "s~dashcore-${OLD_VERSION}-~dashcore-${NEW_VERSION}-~g" {} +
+find . -iname "*.rst" -exec sed -i "s~dashcore-${OLD_VERSION}~dashcore-${NEW_VERSION}~g" {} +
