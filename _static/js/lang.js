4d61faed9b53c5edec7b028dad2877f271082c12
thephez <thephez@users.noreply.github.com>
2023-12-19 15:25:20 -0500

    refactor: update lang.js to work with zh again

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -25,10 +25,23 @@ $(document).ready(function() {
 
 $(function(){
 	var siteURL = "https://docs.dash.org/";
+
+	// Function to convert language codes
+	function convertLangCode(lang) {
+		const langMapping = {
+			'zh-CN': 'zh-cn',
+			'zh-TW': 'zh-tw'
+		};
+		return langMapping[lang] || lang;
+	}
+
 	$("#langselect").on('change', function() {
 		var pageURL = $(location).attr("href");
-		var newLang = $('#langselect').val();
-		var currentLang = DOCUMENTATION_OPTIONS['LANGUAGE'];
+
+		// Convert language codes
+		var siteLang = DOCUMENTATION_OPTIONS['LANGUAGE'];
+		var currentLang = convertLangCode(siteLang);
+		var newLang = convertLangCode($('#langselect').val());;
 
 		pageURL = pageURL.replace(siteURL + currentLang, "");
 		var newFullURL = siteURL + newLang + pageURL;
