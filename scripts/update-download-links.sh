e5ef16930f2999ac1946b6833e88e19c48a9788a
thephez <thephez@users.noreply.github.com>
2024-01-15 09:19:02 -0500

    chore: update download links for v20.0.4

--- scripts/update-download-links.sh
+++ scripts/update-download-links.sh
@@ -4,8 +4,8 @@
 # Dash Core version is released
 
 # Define old and new version variables
-OLD_VERSION="20.0.2"
-NEW_VERSION="20.0.3"
+OLD_VERSION="20.0.3"
+NEW_VERSION="20.0.4"
 
 # Use the variables in the find/sed commands
 find . -iname "*.rst" -exec sed -i "s~/v${OLD_VERSION}/dashcore-${OLD_VERSION}-~/v${NEW_VERSION}/dashcore-${NEW_VERSION}-~g" {} +
