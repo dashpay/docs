14306214390373f6a24ecee3bfeb5aef26fc5e87
thephez <thephez@users.noreply.github.com>
2024-10-23 13:06:51 -0400

    fix: make core version update action work properly (#406)
    * ci: test action
    
    * fix: make find work properly
    
    * ci: debug
    
    * chore: remove test and add comment

--- scripts/core-download-link-update.sh
+++ scripts/core-download-link-update.sh
@@ -12,15 +12,14 @@ NEW_VERSION=$(curl -s \
 if [[ $? -ne 0 || -z "$NEW_VERSION" ]]; then
   echo "Error: Unexpected response when retrieving the current Dash Core version. Received: $NEW_VERSION"
 else
-
   # Print the extracted values (for verification)
   echo "Extracted Version: $NEW_VERSION"
-  git checkout -b v$NEW_VERSION-links
+  # git checkout -b v$NEW_VERSION-links # Uncomment to use locally
 
   # Use the variables in the find/sed commands
-  find . -iname "*.rst" -path "./compiling.rst" -exec sed -i "s~/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~/v${NEW_VERSION}/dashcore-${NEW_VERSION}-~g" {} +
-  find . -iname "*.rst" -path "./compiling.rst" -exec sed -i "s~dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~dashcore-${NEW_VERSION}-~g" {} +
-  find . -iname "*.rst" -path "./compiling.rst" -exec sed -i "s~dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}~dashcore-${NEW_VERSION}~g" {} +
+  find . -iname "*.rst" ! -name "compiling.rst" -exec sed -i "s~/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~/v${NEW_VERSION}/dashcore-${NEW_VERSION}-~g" {} +
+  find . -iname "*.rst" ! -name "compiling.rst" -exec sed -i "s~dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~dashcore-${NEW_VERSION}-~g" {} +
+  find . -iname "*.rst" ! -name "compiling.rst" -exec sed -i "s~dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}~dashcore-${NEW_VERSION}~g" {} +
 
   echo "Dash Core version updated to ${NEW_VERSION} in documentation"
 fi
