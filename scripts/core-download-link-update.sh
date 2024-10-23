dc9bc91dae80153eae38890cd6b83e54d7df7487
thephez <thephez@users.noreply.github.com>
2024-10-23 12:14:49 -0400

    feat: add action to create link update pull requests (#404)
    * feat: update core version update script
    
    Get the latest tag directly from GitHub. Excludes compiling.rst which shows sig check example that shouldn't be modified
    
    * refactor: rename script
    
    * ci: add action to create link update pull requests

---
+++ scripts/core-download-link-update.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to update the download/install related version references when a new
+# Dash Core version is released
+
+# Command to get latest Dash Core tag name
+NEW_VERSION=$(curl -s \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/dashpay/dash/releases/latest | \
+  jq -r '.tag_name' | sed 's/^v//')
+
+if [[ $? -ne 0 || -z "$NEW_VERSION" ]]; then
+  echo "Error: Unexpected response when retrieving the current Dash Core version. Received: $NEW_VERSION"
+else
+
+  # Print the extracted values (for verification)
+  echo "Extracted Version: $NEW_VERSION"
+  git checkout -b v$NEW_VERSION-links
+
+  # Use the variables in the find/sed commands
+  find . -iname "*.rst" -path "./compiling.rst" -exec sed -i "s~/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~/v${NEW_VERSION}/dashcore-${NEW_VERSION}-~g" {} +
+  find . -iname "*.rst" -path "./compiling.rst" -exec sed -i "s~dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~dashcore-${NEW_VERSION}-~g" {} +
+  find . -iname "*.rst" -path "./compiling.rst" -exec sed -i "s~dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}~dashcore-${NEW_VERSION}~g" {} +
+
+  echo "Dash Core version updated to ${NEW_VERSION} in documentation"
+fi
