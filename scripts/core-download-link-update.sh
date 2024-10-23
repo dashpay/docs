1daff5143b2418716580edee7fffce6a8eaa9a92
thephez <thephez@users.noreply.github.com>
2024-10-23 13:02:49 -0400

    chore: remove test and add comment

--- scripts/core-download-link-update.sh
+++ scripts/core-download-link-update.sh
@@ -12,10 +12,9 @@ NEW_VERSION=$(curl -s \
 if [[ $? -ne 0 || -z "$NEW_VERSION" ]]; then
   echo "Error: Unexpected response when retrieving the current Dash Core version. Received: $NEW_VERSION"
 else
-  NEW_VERSION=0.0.1 # test
   # Print the extracted values (for verification)
   echo "Extracted Version: $NEW_VERSION"
-  # git checkout -b v$NEW_VERSION-links
+  # git checkout -b v$NEW_VERSION-links # Uncomment to use locally
 
   # Use the variables in the find/sed commands
   find . -iname "*.rst" ! -name "compiling.rst" -exec sed -i "s~/v[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}/dashcore-[0-9]\{1,\}\.[0-9]\{1,\}\.[0-9]\{1,\}-~/v${NEW_VERSION}/dashcore-${NEW_VERSION}-~g" {} +
