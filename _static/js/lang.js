8183d1fe42820fea28eb57e6dd846ec816e51d75
thephez <thephez@users.noreply.github.com>
2024-12-04 15:40:34 -0500

    chore: remove commented out code

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -1,26 +1,6 @@
 $(document).ready(function() {
 	/* Select current language */
 	$('#langselect').val(DOCUMENTATION_OPTIONS['LANGUAGE']);
-	
-	/* Set alternate links 
-	var langs = [ "de", "en", "es", "fr", "pt", "vi", "el", "ru", "ko", "ja", "zh-Hans", "zh-Hant", "ar", "x-default" ];
-	var pageURL = $(location).attr("href");
-	pageURL = pageURL.replace("https://docs.dash.org/" + DOCUMENTATION_OPTIONS['LANGUAGE'] , "");
-	$.each(langs, function(index, value) {
-		var link = document.createElement('link');
-		link.rel = "alternate";
-		link.hreflang = value;
-		if (value == "x-default") {
-			link.href = "https://docs.dash.org/en" + pageURL;
-		} else if (value == "zh-Hans") {
-			link.href = "https://docs.dash.org/zh_CN" + pageURL;
-		} else if (value == "zh-Hant") {
-			link.href = "https://docs.dash.org/zh_TW" + pageURL;
-		} else {
-			link.href = "https://docs.dash.org/" + value + pageURL;
-		}
-		jQuery('head').append(link);
-	});*/
 });
 
 $(function(){
