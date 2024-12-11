7259cfb160aa20726600b09999c12c163d806f39
thephez <thephez@users.noreply.github.com>
2024-12-11 15:14:36 -0500

    feat: selectively hide language selector (#442)
    * chore: remove commented out code
    
    * feat: selectively hide language selector
    
    * fix: check for excluded path properly

--- _static/js/lang.js
+++ _static/js/lang.js
@@ -1,26 +1,23 @@
 $(document).ready(function() {
 	/* Select current language */
 	$('#langselect').val(DOCUMENTATION_OPTIONS['LANGUAGE']);
-	
-	/* Set alternate links 
-	var langs = [ "de", "en", "es", "fr", "pt", "vi", "el", "ru", "ko", "ja", "zh-Hans", "zh-Hant", "ar", "x-default" ];
-	var pageURL = $(location).attr("href");
-	pageURL = pageURL.replace("https://docs.dash.org/" + DOCUMENTATION_OPTIONS['LANGUAGE'] , "");
-	$.each(langs, function(index, value) {
-		var link = document.createElement('link');
-		link.rel = "alternate";
-		link.hreflang = value;
-		if (value == "x-default") {
-			link.href = "https://docs.dash.org/en" + pageURL;
-		} else if (value == "zh-Hans") {
-			link.href = "https://docs.dash.org/zh_CN" + pageURL;
-		} else if (value == "zh-Hant") {
-			link.href = "https://docs.dash.org/zh_TW" + pageURL;
-		} else {
-			link.href = "https://docs.dash.org/" + value + pageURL;
-		}
-		jQuery('head').append(link);
-	});*/
+
+	// Define paths where the language selector should be hidden
+	var excludePattern = [
+		"/docs/core/"
+	];
+
+	// Get the current path of the page
+	var pagePath = $(location).attr("pathname");
+	console.log(pagePath)
+	// Check if the current page path contains the exclude pattern
+	var shouldHideLangSelector = pagePath.includes(excludePattern);
+
+	if (shouldHideLangSelector) {
+		$('#langselect').hide();
+	} else {
+		$('#langselect').show(); // Ensure it's shown if not excluded
+	}
 });
 
 $(function(){
@@ -41,7 +38,7 @@ $(function(){
 		// Convert language codes
 		var siteLang = DOCUMENTATION_OPTIONS['LANGUAGE'];
 		var currentLang = convertLangCode(siteLang);
-		var newLang = convertLangCode($('#langselect').val());;
+		var newLang = convertLangCode($('#langselect').val());
 
 		pageURL = pageURL.replace(siteURL + currentLang, "");
 		var newFullURL = siteURL + newLang + pageURL;
