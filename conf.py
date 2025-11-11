8b5bd536c53d1dfae9b7c2db6da499c6aeb168dd
thephez <thephez@users.noreply.github.com>
2025-11-11 10:20:02 -0500

    chore: update version in conf

--- conf.py
+++ conf.py
@@ -176,7 +176,7 @@ html_context = {
     # "github_url": "https://github.com", # or your GitHub Enterprise site
     "github_user": "dashpay",
     "github_repo": "docs",
-    "github_version": "22.0.0",
+    "github_version": "23.0.0",
     "doc_path": "",
 }
 
