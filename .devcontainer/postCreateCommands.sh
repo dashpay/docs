13eb5b1c15a5fd06fc7d1f613afcef70a88c7ff3
thephez <thephez@users.noreply.github.com>
2025-11-19 11:32:30 -0500

    Update devcontainer to python 3.13

--- .devcontainer/postCreateCommands.sh
+++ .devcontainer/postCreateCommands.sh
@@ -1,4 +1,6 @@
-#/!bin/sh
+#!/bin/sh
+
+set -eu
 
 # Command to run during postCreateCommand
 # Done in script because when done via object the commands are run in parallel
@@ -6,5 +8,5 @@
 # https://containers.dev/implementors/spec/#parallel-exec
 # https://containers.dev/implementors/json_reference/#formatting-string-vs-array-properties
 
-pip install -r requirements.txt
+python3.13 -m pip install -r requirements.txt
 make html
